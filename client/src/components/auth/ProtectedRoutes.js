2c4003b859f6cb389670fe0689ea420f18bd007f
Thomas Wang <1527thomas@gmail.com>
2020-11-29 01:26:13 -0800

    removed comments in ProtectedRoutes.js

--- client/src/components/auth/ProtectedRoutes.js
+++ client/src/components/auth/ProtectedRoutes.js
@@ -1,18 +1,6 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
 
-// class ProtectedRoute extends React.Component {
-//   render() {
-//     const Component = this.props.component;
-//     const isAuthenticated = localStorage.getItem("token");
-//     return isAuthenticated ? (
-//       <Component />
-//     ) : (
-//       <Redirect to={{ pathname: "/login" }} />
-//     );
-//   }
-// }
-
 function ProtectedRoute({ component }) {
   const isAuthenticated = localStorage.getItem("auth-token");
   console.log(localStorage.getItem("auth-token"));
